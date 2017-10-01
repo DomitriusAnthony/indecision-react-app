c8f9a0a28d4c5911a5a7cb942b84a8065a282197
Domitrius Clark <domitrius.anthony@hotmail.com>
2017-10-01 11:52:47 -0400

    Added loaders for SCSS and connected styles

--- src/app.js
+++ src/app.js
@@ -1,48 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
-
-const Layout = (props) => {
-    return (
-        <div>
-            <p>header</p>
-            {props.children}
-            <p>footer</p>
-        </div>
-    );
-};
+import './styles/styles.scss';
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
 
-
-/*
-
-class OldSyntax{
-    constructor() {
-        this.name = "Mike";
-        this.getGreeting = this.getGreeting.bind(this);
-    }
-    getGreeting() {
-        return `Hi my name is ${this.name}.`;
-    }
-}
-
-
-const oldSyntax = new OldSyntax();
-console.log(oldSyntax.getGreeting() );
-
---------------------------------
-// arrow functions bind the 'this' to the parent
-class NewSyntax {
-    name = 'Jen';
-    get greeting = () => {
-        return `Hi. My name is ${this.name}.`;
-    }
-}
-
-const newSyntax = new NewSyntax();
-const newGetGretting = newSyntax.getGreeting();
-console.log(newGetGreeting;
-
-
-*/
