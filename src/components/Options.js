0dff0a02fe14c3a98698fbcdb3742d4baba083b7
Domitrius Clark <domitrius.anthony@hotmail.com>
2017-10-02 02:03:24 -0400

    Added styling and set up SCSS with webpack

--- src/components/Options.js
+++ src/components/Options.js
@@ -3,17 +3,20 @@ import Option from './Option';
 
 const Options = (props) => (
         <div>
-            <button onClick={props.handleDeleteOptions}>Remove all</button>
-            {props.options.length === 0 && <p>Please add an option to get started</p> }
-            {
-                props.options.map((option) => (
-                    <Option 
-                        key={option} 
-                        optionText={option} 
-                        handleDeleteOption={props.handleDeleteOption}
-                    />
-                ))
-            }
+            <div className="widget-header">
+                <h3 className="widget-header__title">Your options</h3>
+                <button className="button button--link" onClick={props.handleDeleteOptions}>Remove all</button>
+            </div>
+                {props.options.length === 0 && <p className="widget__message">Please add an option to get started</p> }
+                {
+                    props.options.map((option) => (
+                        <Option 
+                            key={option} 
+                            optionText={option} 
+                            handleDeleteOption={props.handleDeleteOption}
+                        />
+                    ))
+                }
         </div>  
 );
 
